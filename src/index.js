82ec16f4280fb764114c0e63b841745fd0f0f384
ALi3TR <ali_sedaghi@ymail.com>
2020-10-31 21:31:56 +0330

    [fix] Change main render to routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from "./Component/Login/Login";
-import Signup from "./Component/Signup/Signup"
+import Signup from "./Component/Signup/Signup";
+import Routes from "./Routes";
 import 'antd/dist/antd.css';
 import './Style/Login.css';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
-//ReactDOM.render(<Signup />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
