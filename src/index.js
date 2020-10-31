05120d7c99248545a83838c63da16c1395861b69
yasmin-mdn <yasmina.md78@gmail.com>
2020-10-31 22:09:28 +0330

    FixEditProfile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
   
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'antd';
+//import { Button } from 'antd';
 //import 'antd/dist/antd.css';
 //import EditProfile from "./Component/EditProfile/EditProfile.js"
 import App from "./Component/EditProfile/EditProfile.js"
