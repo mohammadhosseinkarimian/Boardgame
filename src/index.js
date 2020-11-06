92e7c6059460f88a5de4136aaf1ab29e62b56fd1
mohammadhosseinkarimian <mohammadkarimian122@gmail.com>
2020-11-06 06:17:23 +0330

    empty commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import Home from './Component/homepage';
 import Login from "./Component/Login/Login";
 import Signup from "./Component/Signup/Signup";
-import Home from './component/homepage';
 import 'antd/dist/antd.css';
 import Routes from "./Routes";
-import 'antd/dist/antd.css';
-import './Style/Login.css';
+import './style/Login.css'
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
