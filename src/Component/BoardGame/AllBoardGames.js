96d2b80a2d02a3a576f9c0f22e9c5459590fc9c5
yasmin-mdn <yasmina.md78@gmail.com>
2020-11-08 19:24:00 +0330

    '[change]AllGameBoards'

---
+++ src/Component/BoardGame/AllBoardGames.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Axios from "axios";
+import {Link} from "react-router-dom";
+import 'antd/dist/antd.css';
+import {
+  Form,
+  Input,
+  Tooltip,
+  Select,
+  AutoComplete,
+  Button,
+  Collapse,
+  Card ,
+  List,
+  Avatar, 
+  Skeleton,
+  Pagination 
+} from "antd";
+
+
+class AllBoardGames extends React.Component {
+  state={
+      id:"",
+      name:"",
+      description:"",
+      category:"",
+      image:"",
+      min_players:"",
+      max_players:"",
+      difficulty:"",
+      proxyurl : "",
+      rate:"",
+      games: []
+      
+      
+  };
+  proxyurl="https://cors-anywhere.herokuapp.com/";
+  componentDidMount() {
+    Axios.get(this.proxyurl+'http://gameboard.pythonanywhere.com/game/games_list/')
+       .then(res=>{
+         const games_list=res.data;
+         this.setState(prevState => {
+          console.log(games_list)
+           return {games: games_list}
+         })
+       })
+  }
+
+  
+  render(){
+    return(
+    <div>
+    <List
+    itemLayout="horizontal"
+    dataSource={this.state.games}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src={item.image} />}
+          title={<Link to={`/boardgames/${item.id}`}>{item.name}</Link>}
+          description={`rate:${item.rate}`}
+        />
+      </List.Item>
+    )}
+  />
+  </div>
+  );}
+  }
+  export default AllBoardGames;
+  
+  
