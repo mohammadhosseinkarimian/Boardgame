29e07661b3061eb3b47ea2d10f9e1a2654f36dd4
mohammadhosseinkarimian <47971866+mohammadhosseinkarimian@users.noreply.github.com>
2020-11-16 10:29:39 +0330

    Cart for every game

--- src/Component/BoardGame/SingleGame.js
+++ src/Component/BoardGame/SingleGame.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Axios from "axios";
 import { Link } from "react-router-dom";
+import './boardstyle.css'
 import 'antd/dist/antd.css';
 import {
     Form,
@@ -17,47 +18,70 @@ import {
     Pagination
 } from "antd";
 
+import 'font-awesome/css/font-awesome.min.css';
+
 const { Meta } = Card;
 const { Panel } = Collapse;
 class SingleGame extends React.Component {
     state = {
-        id: "6",
-        name: "Die Macher",
-        description: "Die Macher is a game about seven sequential political races in different mmmmmmjjjjuuisiadddsijdalisdjsjidnjklivf sijdv fikgdihgfjjfgjhhjdfjfjfgjkgijgffjeejeirfifdsiiwiwiiiejejddcdcvjsdowirpoeutjrifvnjjkghfkjhnlifkeiueij",
-        category: "action",
-        image: "https://cf.geekdo-images.com/original/img/uqlrq_bQJqHpcaN7_7qocV5XfbU=/0x0/pic4718279.jpg",
-        min_players: "3",
-        max_players: "5",
-        difficulty: "2.1505",
-        rate: "7.63088",
+        id: localStorage.getItem('game'),
+        name: "",
+        description: "",
+        category: "",
+        image: "",
+        min_players: "",
+        max_players: "",
+        difficulty: "",
+        rate: "",
     };
 
     componentDidMount(){
-       const id=6
-    Axios.get(this.proxyurl+'http://gameboard.pythonanywhere.com/game/game_info/'+id)
+       const id=window.location.href.substring(32);
+    Axios.get('http://localhost:8010/proxy/game/game_info/'+id)
    .then(res=>{
-     const game=res.data
-     console.log(game)
+     const game=res.data;
+     this.setState({name:game.name});
+     this.setState({description:game.description});
+     this.setState({category:game.category});
+     this.setState({image:game.image});
+     this.setState({min_players:game.min_players});
+     this.setState({max_players:game.max_players});
+     this.setState({difficulty:game.difficulty});
+     this.setState({rate:game.rate});
+
+
+
     })
 }
 
     render() {
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"></link>
         return (
-            <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt={this.state.name} src={this.state.image} />}
-            >
-                <Meta title={this.state.name} description={this.state.description} />
+            <div
+                style={{ width: "100%", background: '272727'}}>
+                    <div style={{width:"60%",marginTop:"5%",marginLeft: "25%"}}>
+                    <img style={{width:"60%",height: '43vh',marginTop:"0vh",float: 'left',marginRight: '1rem'}} 
+                    src={this.state.image} />
+                   <br/>
+                   <h4 style={{fontSize: '21',color: 'white'}}>{"Name: "+this.state.name }</h4>
+                    <p style={{fontSize: '17px',color: 'whitesmoke',marginLeft: '5%'}}>{`${this.state.rate}`.substring(0,3)}<i className="fa fa-star fa-star" style={{color: 'gold',fontSize: '23'}}/> </p>
+                    <p style={{fontSize: '17px',color: 'whitesmoke',marginLeft: '5%'}}>{`Difficulty: ${this.state.difficulty}`.substring(0,15)}</p>
+                    <p style={{fontSize: '17px',color: 'whitesmoke',marginLeft: '5%'}}>{'Range of players: '+this.state.min_players+" to "+this.state.max_players}</p>
+                    
+
+                    </div>
+                   <div style={{textAlign: 'center',marginTop: '10%'}}>
+
+                       
+                   </div>
+                   <h5 style={{fontSize: '16px',color: 'whitesmoke',marginLeft: '1%',marginTop: '3%'}}  >{"Categories: "+ this.state.category}</h5>
+                 
                 <Collapse  bordered={false} >
                     <Panel header="more" showArrow={false}>
-                    <p><h3>category: </h3> {this.state.category}</p>
-                    <p><h3>number of players: </h3> {this.state.min_players} to {this.state.max_players}</p>
-                    <p><h3>difficulty: </h3> {this.state.difficulty}</p>
-                    <p><h3>rate: </h3> {this.state.rate}</p>
+                    <h5 style={{fontSize: '15px',color: 'whitesmoke',marginLeft: '0%'}} >{this.state.description}</h5>
                     </Panel>
                 </Collapse>
-            </Card>
+            </div>
         )
     }
 }
