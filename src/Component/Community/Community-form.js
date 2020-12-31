71d3b07283e67536d4a93dba0c350f7ce4417a51
shoeibi98 <shoeibi.s77@gmail.com>
2020-12-31 21:11:30 +0330

    [fix]error members

--- src/Component/Community/Community-form.js
+++ src/Component/Community/Community-form.js
@@ -88,8 +88,9 @@ const formItemLayout = {
   }
   
   onSelectuser = (value) => {
-    var dict = { "username": value }
-    this.state.members.push(dict);
+    this.state.members=[]
+    var dict =[]
+    value.forEach(v=> this.state.members.push({ "username": v}))
     this.setState({ selected_user: value }, () => {
       console.log(this.state.selected_user, 'dealersOverallTotal1')
     })
@@ -150,11 +151,11 @@ const formItemLayout = {
        const data={
         name:this.state.name,
         description:this.state.description,
-        owner:this.state.owner,
+      //  owner:this.state.owner,
         members:this.state.members,
        image:this.state.img,
        lock:this.state.lock,
-       owner:localStorage.getItem('id')
+     //  owner:localStorage.getItem('id')
     }
        if((this.state.name!=="") && (this.state.description!=="")){
       // e.target.reset();
